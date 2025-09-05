d346b78e884020b1648b35f28a783a3cdcf05186
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-09-05 12:52:30 -0400

    "Hello, World!" - "Hello, Scout!"

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: scout mosley
+ * Created on: Sep 2025
+ * This program says hello scout!
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Scout!')
